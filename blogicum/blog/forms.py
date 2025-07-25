d4f91ebd4311e564c528bd0592fd33fbbd9e6aea
ogsvnv <ogsvnv@gmail.com>
2025-07-25 23:32:17 +0400

    Ревью 1.01

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,23 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
-from .models import Post, Comment
+
+from .models import Comment, Post
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text', 'image', 'category', 'location', 'is_published', 'pub_date')
+        fields = ('title', 'text', 'image', 'category', 'location', 'is_published',
+                  'pub_date')
+
 
 class UserForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
 
 class EditUserForm(forms.ModelForm):
     class Meta:
